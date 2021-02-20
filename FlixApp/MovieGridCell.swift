cd7cd56efe68d442c1f022c374fab5711aa350e8
MediumBen <56977679+MediumBen@users.noreply.github.com>
2021-02-19 23:43:45 -0500

    Add files via upload

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Benjamin Izrailov on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+     
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
